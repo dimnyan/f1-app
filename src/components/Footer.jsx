a930b39a4ac8aea8ae48a44056a4f8665063dfcf
Nyoman Lanang Kresna Erlangga <dimasnyoman1@gmail.com>
2023-10-03 22:23:56 +0700

    Update Footer.jsx
    footer credit ergast API

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,58 +1,71 @@
 function Footer() {
   return (
-    <footer className="text-white py-6 bg-gray-900  top-0 left-0 border-b border-gray-900 ">
-      <div className="px-7 pt-2 pb-10 lg:w-2/3 mx-auto">
-        <h2 className="font-bold md:mx-auto text-lg w-max pb-3 uppercase">
-          My Socials
-        </h2>
-        <ul className="w-min md:flex md:gap-10 md:mx-auto md:pt-3">
-          <li className="w-min pb-1">
-            <a
-              className="font-bold underline w-max tracking-wider"
-              href="https://www.github.com/dimnyan"
-            >
-              Github
-            </a>
-          </li>
-          <li className="w-min pb-1">
-            <a
-              className="font-bold underline w-max tracking-wider"
-              href="https://www.linkedin.com/in/nyomanlanang"
-            >
-              LinkedIn
-            </a>
-          </li>
-          <li className="w-min pb-1">
-            <a
-              className="font-bold underline w-max tracking-wider"
-              href="https://www.instagram.com/dimnyan"
-            >
-              Instagram
-            </a>
-          </li>
-          <li className="w-min pb-1">
-            <a
-              className="font-bold underline w-max tracking-wider"
-              href="https://www.twitter.com/dimnyomm"
-            >
-              Twitter
-            </a>
-          </li>
-        </ul>
-      </div>
-      <div className="text-center">
-        &copy; 2023&#160;
+    <>
+      <footer className="text-white pb-6 pt-3 bg-gray-900  top-0 left-0 border-b border-gray-900 ">
+        <div className="px-7 pt-2 pb-8 md:pb-6 lg:w-2/3 mx-auto">
+          <h2 className="font-bold mx-auto text-lg w-max pb-3 uppercase">
+            My Socials
+          </h2>
+          <ul className="w-min md:flex md:gap-10 mx-auto md:pt-5">
+            <li className="w-min pb-4 pt-3 md:pt-0 mx-auto">
+              <a
+                className="font-bold underline w-max tracking-wider"
+                href="https://www.github.com/dimnyan"
+              >
+                Github
+              </a>
+            </li>
+            <li className="w-min pb-4 mx-auto">
+              <a
+                className="font-bold underline w-max tracking-wider"
+                href="https://www.linkedin.com/in/nyomanlanang"
+              >
+                LinkedIn
+              </a>
+            </li>
+            <li className="w-min pb-4 mx-auto">
+              <a
+                className="font-bold underline w-max tracking-wider"
+                href="https://www.instagram.com/dimnyan"
+              >
+                Instagram
+              </a>
+            </li>
+            <li className="w-min pb-4 mx-auto">
+              <a
+                className="font-bold underline w-max tracking-wider"
+                href="https://www.twitter.com/dimnyomm"
+              >
+                Twitter
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="text-center">
+          &copy; 2023&#160;
+          <a
+            href="https://dimnyan.tech"
+            rel="noopener noreferrer"
+            target="_blank"
+            className=" underline"
+          >
+            dimnyan
+          </a>
+          . All rights reserved.
+        </div>
+      </footer>
+      <div className="text-center w-full">
+        Powered by{" "}
         <a
-          href="https://dimnyan.tech"
-          rel="noopener noreferrer"
+          href="http://ergast.com/mrd/"
+          className="underline"
           target="_blank"
-          className=" underline"
+          rel="noreferrer"
         >
-          dimnyan
+          Ergast API
         </a>
-        . All rights reserved.
       </div>
-    </footer>
+    </>
   );
 }
 
